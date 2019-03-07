803cf65f502527c91871ff4fd922d09b404d7e68
Đặng Xuân Vương <vuong.1998.school.01@gmail.com>
2019-03-08 02:32:35 +0700

    Update train.py

--- train.py
+++ train.py
@@ -15,7 +15,7 @@ from PIL import ImageFile
 
 def _main():
     annotation_path = 'train.txt'
-    log_dir = 'logs/000_1/'
+    log_dir = 'logs/000_2/'
     classes_path = 'model_data/pineapple_classes.txt'
     anchors_path = 'model_data/pineapple_anchors.txt'
     class_names = get_classes(classes_path)
@@ -30,7 +30,7 @@ def _main():
             freeze_body=2, weights_path='model_data/tiny_yolo_weights.h5')
     else:
         model = create_model(input_shape, anchors, num_classes,
-            freeze_body=2, weights_path='model_data/yolo_weights.h5') # make sure you know what you freeze
+            freeze_body=2, weights_path='model_data/trained_weights_stage_1.h5') # make sure you know what you freeze
 
     logging = TensorBoard(log_dir=log_dir)
     checkpoint = ModelCheckpoint(log_dir + 'ep{epoch:03d}-loss{loss:.3f}-val_loss{val_loss:.3f}.h5',
@@ -60,10 +60,10 @@ def _main():
                 steps_per_epoch=max(1, num_train//batch_size),
                 validation_data=data_generator_wrapper(lines[num_train:], batch_size, input_shape, anchors, num_classes),
                 validation_steps=max(1, num_val//batch_size),
-                epochs=10,
-                initial_epoch=0,
+                epochs=20,
+                initial_epoch=10,
                 callbacks=[logging, checkpoint])
-        model.save_weights(log_dir + 'trained_weights_stage_1.h5')
+        model.save_weights(log_dir + 'trained_weights_stage_2.h5')
 
     # Unfreeze and continue training, to fine-tune.
     # Train longer if the result is not good.
